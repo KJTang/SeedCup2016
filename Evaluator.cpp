2fcea9802234cb2e363b90c4ad486f3b18a4927d
Sun Peng <sunpeng1995@gmail.com>
2016-10-26 20:20:31 +0800

    feat: evaluate & environment

---
+++ Evaluator.cpp
@@ -0,0 +1,65 @@
+﻿#include "Evaluator.h"
+
+Evaluator::Evaluator(ASTNode* root) {
+	// TODO
+}
+
+template<typename T>
+Environment<T>::Environment() {
+	scopes_.push_back(unordered_map<string, T>());
+}
+
+template<typename T>
+bool Environment<T>::getin_new_scope() {
+	scopes_.push_back(unordered_map<string, T>());
+	cur_scope_ += 1;
+	return true;
+}
+
+template<typename T>
+bool Environment<T>::getout_a_scope() {
+	if (cur_scope_ == 0) {
+		return false;
+	}
+	scopes_.erase(scopes_.begin() + cur_scope_);
+	cur_scope_ -= 1;
+	return true;
+}
+
+template<typename T>
+bool Environment<T>::is_var_exist(string var_name) {
+	return is_var_exist(var_name, cur_scope_);
+}
+
+template<typename T>
+bool Environment<T>::is_var_exist(string var_name, int scope_layer) {
+	if (scope_layer < 0) {
+		return false;
+	}
+	if (scopes_[scope_layer].count(var_name)) {
+		return true;
+	}
+	return is_var_exist(var_name, scope_layer - 1);
+}
+
+template<typename T>
+T Environment<T>::lookup_var(string var_name) {
+	return lookup_var(var_name, cur_scope_);
+}
+
+template<typename T>
+T Environment<T>::lookup_var(string var_name, int scope_layer) {
+	if (scope_layer < 0) {
+		return T();
+	}
+	auto ret = scopes_[scope_layer].find(var_name);
+	if (ret != scopes_[scope_layer].end()) {
+		return ret->second;
+	}
+	return lookup_var(var_name, scope_layer - 1);
+}
+
+template<typename T>
+bool Environment<T>::add_var(string var_name, T value) {
+	scopes_[cur_scope_][var_name] = value;
+}
---
+++ Evaluator.h
@@ -0,0 +1,36 @@
+﻿#ifndef __EVALUATOR_H__
+#define __EVALUATOR_H__
+
+#include <vector>
+#include <unordered_map>
+#include "astnode.h"
+
+using namespace std;
+
+class Evaluator {
+public:
+	Evaluator(ASTNode* root);
+
+private:
+	Environment<int> env_;
+};
+
+template<typename T>
+class Environment {
+public:
+	Environment() : cur_scope(0);
+
+	bool is_var_exist(string var_name);
+	T lookup_var(string var_name);
+	bool add_var(string var_name, T value);
+
+	bool getin_new_scope();
+	bool getout_a_scope();
+private:
+	vector<unordered_map<string, T>> scopes_;
+	bool is_var_exist(string var_name, int scope_layer);
+	T lookup_var(string var_name, int scope_layer);
+	int cur_scope_;
+};
+
+#endif // !__EVALUATOR_H__

634507d55c5bbb726d86a5c4e3843d8ddf2968e4
Sun Peng <sunpeng1995@gmail.com>
2016-11-06 09:59:42 +0800

    project: add cppjieba library and setup

---
+++ finals/src/main.cpp
@@ -0,0 +1,29 @@
+﻿#include "tokenizer.h"
+
+using namespace std;
+
+const char* const DICT_PATH = "../cppjieba/dict/jieba.dict.utf8";
+const char* const HMM_PATH = "../cppjieba/dict/hmm_model.utf8";
+const char* const USER_DICT_PATH = "../cppjieba/dict/user.dict.utf8";
+const char* const IDF_PATH = "../cppjieba/dict/idf.utf8";
+const char* const STOP_WORD_PATH = "../cppjieba/dict/stop_words.utf8";
+
+int main() {
+  cppjieba::Jieba jieba(DICT_PATH,
+    HMM_PATH,
+    USER_DICT_PATH,
+    IDF_PATH,
+    STOP_WORD_PATH);
+  vector<string> words;
+  vector<cppjieba::Word> jiebawords;
+  string s;
+  string result;
+
+  s = "他来到了网易杭研大厦";
+  cout << s << endl;
+  cout << "[demo] Cut With HMM" << endl;
+  jieba.Cut(s, words, true);
+  cout << limonp::Join(words.begin(), words.end(), "/") << endl;
+
+  return 0;
+}
---
+++ finals/src/tokenizer.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#ifndef __TOKENIZER_H__
+#define __TOKENIZER_H__
+#include <cppjieba/Jieba.hpp>
+
+
+
+
+
+#endif // !__TOKENIZER_H__

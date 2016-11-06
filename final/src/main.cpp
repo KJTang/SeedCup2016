54d9d0de5f21043b4d0d953c089d74a8b26752de
Sun Peng <sunpeng1995@gmail.com>
2016-11-06 10:08:36 +0800

    project: clean

--- final/src/main.cpp
+++ final/src/main.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+﻿#include <iostream>
 
 #include "analyzer.h"
 #include "product.h"
+#include "tokenizer.h"
+
+using namespace std;
+
+const char* const DICT_PATH = "../cppjieba/dict/jieba.dict.utf8";
+const char* const HMM_PATH = "../cppjieba/dict/hmm_model.utf8";
+const char* const USER_DICT_PATH = "../cppjieba/dict/user.dict.utf8";
+const char* const IDF_PATH = "../cppjieba/dict/idf.utf8";
+const char* const STOP_WORD_PATH = "../cppjieba/dict/stop_words.utf8";
 
 int main(int argc, char const *argv[]) {
     Product p1, p2;
@@ -10,6 +19,21 @@ int main(int argc, char const *argv[]) {
     Analyzer analyzer;
     double result = analyzer.analysis(p1, p2);
     std::cout<<"Result: "<<result<<std::endl;
+
+    cppjieba::Jieba jieba(DICT_PATH,
+      HMM_PATH,
+      USER_DICT_PATH,
+      IDF_PATH,
+      STOP_WORD_PATH);
+    vector<string> words;
+    vector<cppjieba::Word> jiebawords;
+    string s;
+
+    s = "他来到了网易杭研大厦";
+    cout << s << endl;
+    cout << "[demo] Cut With HMM" << endl;
+    jieba.Cut(s, words, true);
+    cout << limonp::Join(words.begin(), words.end(), "/") << endl;
 
     return 0;
 }
